9788dde906fb0f3165ecc67bef661800258e1065
Christian Berendt <berendt@osism.tech>
2021-11-13 13:39:08 +0100

    Initial commit
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ README.md
@@ -0,0 +1,5 @@
+# container-image-netbox
+
+[![Quay](https://img.shields.io/badge/Quay-osism%2Fnetbox-blue.svg)](https://quay.io/repository/osism/netbox)
+
+The container image is based on the community container image and extends it with useful plugins that we use within OSISM.
---
+++ scripts/push.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -x
+
+# Available environment variables
+#
+# DOCKER_REGISTRY
+# REPOSITORY
+# VERSION
+
+DOCKER_REGISTRY=${DOCKER_REGISTRY:-quay.io}
+REVISION=$(git rev-parse HEAD)
+
+if [[ -n $DOCKER_REGISTRY ]]; then
+    REPOSITORY="$DOCKER_REGISTRY/$REPOSITORY"
+fi
+
+buildah login --password $DOCKER_PASSWORD --username $DOCKER_USERNAME $DOCKER_REGISTRY
+
+buildah tag "$REPOSITORY:$REVISION" "$REPOSITORY:$VERSION"
+buildah push "$REPOSITORY:$VERSION"
